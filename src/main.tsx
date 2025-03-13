276a1fe04f79c78506d251bb93233a9e56d74344
GuillemS <guim.san.oli@gmail.com>
2025-03-13 19:20:12 +0100

    #1 Implement translations in layout and install the dependencies for i18n

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,31 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Layout from "./Layout.tsx";
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
+
+import global_es from "./translations/es/global.json";
+import global_en from "./translations/en/global.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "es",
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Layout>
-      <App />
-    </Layout>
-  </StrictMode>
+  <I18nextProvider i18n={i18next}>
+    <StrictMode>
+      <Layout>
+        <App />
+      </Layout>
+    </StrictMode>
+  </I18nextProvider>
 );
